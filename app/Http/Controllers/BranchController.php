aa766508f459d63d012af0b4c3f684dc1ecf7137
bishal221973 <bishalcodeslaravel@gmail.com>
2026-08-22 15:33:38 +0545

    Wrp

--- app/Http/Controllers/BranchController.php
+++ app/Http/Controllers/BranchController.php
@@ -11,8 +11,8 @@ class BranchController extends Controller
     public function index(Request $request)
     {
         $branches = Organization::where('parent_id', 1)->latest()->get();
-        $branches1=Organization::query()
-    ->dataTablePaginate();
+        $branches1 = Organization::query()
+            ->dataTablePaginate();
         return Inertia::render('Admin/Manage/Branch/Index', [
             'branches' => $branches,
             'branches1' => $branches1
